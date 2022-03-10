be6854712fd4b2d8ad6689d03324f71aa85fa7c7
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-10 06:09:49 -0800

    prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,7 @@
 
 
 /**
- *
- * main - prints the phrase with proper grammar, but the outcome is a piece of art,
- *
+ * main - prints the phrase with proper grammar, but the outcome  *is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
 
@@ -13,6 +11,6 @@ int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n"); 
 		return (0);
 }
