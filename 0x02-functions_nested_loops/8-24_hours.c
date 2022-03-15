fafa2902ae8c5fc6e6c0257f84f7606f29b5de54
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-15 11:31:22 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -30,13 +30,14 @@ void jack_bauer(void)
 				m1 = 0;
 				while (m1 <= 9)
 				{
+				
+					_putchar('0' + h2);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m2);
+					_putchar('0' + m1);
+					_putchar('\n');
 
-	_putchar('0' + h2);
-	_putchar('0' + h1);
-	_putchar(':');
-	_putchar('0' + m2);
-	_putchar('0' + m1);
-	_putchar('\n');
 					m1++;
 				}
 				m2++;
