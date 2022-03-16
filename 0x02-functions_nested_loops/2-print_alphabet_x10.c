184c35a9d4798a2be6699a5219ae2526ec14c12a
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-16 05:17:16 -0700

    task 2 edit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet ten times followed by new line
+ * print_alphabet_x10 - print alphabet ten times followed by new line
  *
  * Return: 0
  */
