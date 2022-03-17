24cc8ec5e92e4bc74a5161cee98ff80ffc01c866
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-17 06:44:29 -0700

    task 4 edit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,17 +5,16 @@
  * except 2 and 4, followed by new line
  */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != 50 && i != 52)
 		{
-			_putchar(i + '0');
+			_putchar(i);
 		}
 	}
-
 	_putchar('\n');
 }
