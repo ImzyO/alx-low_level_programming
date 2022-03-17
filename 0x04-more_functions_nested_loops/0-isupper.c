6614c7f6622124a398df86b6632f0e3021ea39ca
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-17 06:17:42 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for uppercase character
  *
- * @c: the character to be checked
+ * @c: character to be checked
  *
  * Return: Always 1 if c is uppercase, 0 otherwise
  */
@@ -15,8 +15,5 @@ int _isupper(int c)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
