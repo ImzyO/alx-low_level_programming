2b067424e3dc8b045e189ecb387846c3f6b73104
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-17 03:06:12 -0700

    task 12 edit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
+ * main - prints all possible combinations of,
+ * two two-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -19,7 +20,8 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 98 || j !=99)
+
+			if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
@@ -28,5 +30,6 @@ int main(void)
 		}
 	}
 	putchar('n');
+
 	return (0);
 }
