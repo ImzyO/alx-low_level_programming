fab9ac6f1adc4e70277aaba05aa5b318cf76ada3
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-21 06:24:27 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_array - print n elements of an array of integers
+ * followed by new line
+ * @a: Array
+ * @n: Number of array
+ * Return: Always 0 (Success)
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; n > i; i++)
+		{
+			if (i != n - 1)
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+				printf("%d", a[i]);
+			}
+		}
+	}
+	printf("\n");
+}
