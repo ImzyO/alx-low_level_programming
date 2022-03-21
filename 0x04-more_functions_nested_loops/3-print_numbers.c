81c326fd3d37c37423776898f72cd9fbe1648b6a
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-21 10:13:07 -0700

    task3edit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - print numbers 0 to 9, followed by new line
+ * print_numbers - print numbers 0 to 9, followed by new lin
+ *Return: Always 0 (Success)
  */
 
 void print_number(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
