a9cfa9074c09504d14969d3dd9d235900431c47f
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-22 05:48:05 -0700

    task 11 edit 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,8 +25,8 @@ int main(void)
 
 						putchar(n);
 						putchar(m);
-						putchar(l);	
-						if (n != 55 || m != 56)
+						putchar(l);
+					if (n != 55 || m != 56)
 						{
 							putchar(',');
 							putchar(' ');
