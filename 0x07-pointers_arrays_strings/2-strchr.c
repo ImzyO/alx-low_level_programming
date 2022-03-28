3cb09b448d2db9de33edeace7f6f81b5b224946a
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-28 03:35:18 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates character in string
+ * @s: buffer
+ * @c: buffer2
+ *
+ * Return: * to 'c' firts occurence, NULL otherwise
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (&s != c)
+	{
+		if (!*s++)
+		{
+			return (0);
+		}
+	}
+	return (s);
+}
