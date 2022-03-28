d9079c3c37f96d88371c59bc6e9cdbdfaf28ae74
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-28 06:23:11 -0700

    task 1 edit 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,26 +11,22 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, c = 0, s = 0;
+	int i, j;
+
+	i = 0;
+	j = 0;
 
 	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
 	{
-		c++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
-	while (s <= c)
-	{
-		if (n > 0)
-		{
-			dest[i] = src[s];
-			s++;
-			i++;
-			n--;
-		}
-		else
-		{
-			break;
-		}
-	}
+	dest[i] = '\0';
+
 	return (dest);
 }
