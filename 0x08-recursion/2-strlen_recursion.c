01213959a53f9acd5a67e782ba6ad3214e95b8ba
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-03-30 06:55:45 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strln_recursion - returns length of a string
+ * @s: character of string
+ * Return: length of string, otherwise 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*(s) != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
