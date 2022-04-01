394f95242065b020cd438453fb1eeba974dbe6d2
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-04-01 10:22:14 -0700

    added c files

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - write character to std.out
+ * @c: character to print
+ *
+ * Return: 1 success, -1 error
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
