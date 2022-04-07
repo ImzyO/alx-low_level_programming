65e335089ac13838127eb16ec874198dd556ff09
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-04-07 04:45:49 -0700

    memory allocated

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory
+ *
+ * Return: Always 0 (Success)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *i
+	
+	i = malloc(b);
+
+	if (i == 0)
+	{
+		exit(98);
+	}
+	return (i);
+}
