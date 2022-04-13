4d7d8127ba3ce0aad1eef6d67472c7a82e96fef3
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-04-13 03:59:43 -0700

    added files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - write a program that prints name of the file,
+ * it was compiled from then new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
