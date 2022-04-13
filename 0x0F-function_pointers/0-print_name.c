8a13c9617d7fdf64c4c8c5757c92af52287a42db
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-04-13 01:10:01 -0700

    prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be checked
+ * @f: pointer to void
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && F !- NULL)
+		f(name);
+}
