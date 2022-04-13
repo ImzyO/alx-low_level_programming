948a053febca40f79834274cdac16437cc135a6b
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-04-13 01:17:01 -0700

    executes a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as parameter,
+ * on each element of an array
+ * @size: size of array
+ * @action: pointer to the function void
+ * @array: array of elements
+ *
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
