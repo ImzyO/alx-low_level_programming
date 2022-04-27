1118cddf39aa3575cdf6a3d4685005ac27a2d19d
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-04-27 04:23:13 -0700

    edited node added at beginning of list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,25 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-
-/**
- * _strlen - function that returns the length of  astring
- * @s: s is a character
- *
- * Return: value is i
- */
-
-int _strlen(const chhar *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 
 /**
  * add_note - adds a new node at beginning of a list list_t
@@ -31,16 +12,20 @@ int _strlen(const chhar *s)
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *add;
+	list_t *new;
+	unsigned int len = 0;
 
-	add = malloc(sizeof(list_t));
-	if (add == NULL)
-		return (NULL)
-			add->str = strdup(str);
+	while (str[len])
+		len++;
 
-	add->str = _strlen(str);
-	add->next = *head;
-	*head = add;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
-	return (add);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
 }
