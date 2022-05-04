869443ff8bd859a66fd5085ca8c82388c33ac246
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-05-04 02:54:02 -0700

    edited function in header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,12 +4,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_context);
+int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
