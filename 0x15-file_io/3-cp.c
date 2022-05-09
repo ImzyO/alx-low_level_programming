59b18c814cef9455d11c7d8778c01bb59d65dcfe
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-05-09 03:56:00 -0700

    changed positioning of curly bracckets

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,8 +28,7 @@ int main(int ac, char *av[])
 	if (output_fd == -1)
 		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
 
-	do
-       	{
+	do {
 		istatus = read(input_fd, buf, MAXSIZE);
 		if (istatus == -1)
 		{
@@ -42,8 +41,7 @@ int main(int ac, char *av[])
 			if (ostatus == -1)
 				dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
-	} 
-	while (istatus > 0);
+	} while (istatus > 0);
 
 	istatus = close(input_fd);
 	if (istatus == -1)
