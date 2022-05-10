f90efcb3e3d2072bd943e2558310ab905429ddf3
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-05-10 06:19:14 -0700

    testing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
  * main - prints the phrase, with proper grammar,
  * but the outcome is a piece of art,
  * followed by a new line
- * Return: Always 0 (Success)
+ *
+ * Return: 0 Always (Success)
  */
 
 int main(void)
-
 {
 
 		printf("with proper grammar, but the outcome is a piece of art,\n");
