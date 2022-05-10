e2c51ba8fd9dcb3aafc4acf871d98ad8a0f4fbf2
ImzyO <100032916+ImzyO@users.noreply.github.com>
2022-05-10 06:12:06 -0700

    changed entire code block file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,19 +9,18 @@
  */
 
 int main(void)
-
 {
+	int i;
+	char c;
+	long int j;
+	long long int k;
+	float l;
 
-		printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("The size of int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of char is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of long int is: %lu.\n", (unsigned long)sizeof(j));
+	printf("The size of long long int is: %lu.\n", (unsigned long)sizeof(k));
+	printf("The size of float is: %lu.\n", (unsigned long)sizeof(l));
 
-		printf("Size of an int: %d byte(s)\n", sizeof(int));
-
-		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-
-		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-
-		printf("Size of a float: %d byte(s)\n", sizeof(float));
-
-		return (0);
-
+	return (0);
 }
