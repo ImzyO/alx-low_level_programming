0f2f364f8f294a15f4296a98c4a8e16096cbf519
Imani <100032916+ImzyO@users.noreply.github.com>
2022-10-03 12:44:51 +0300

    Jump search in a singly linked list

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,29 +1,30 @@
 #include "search_algos.h"
 #include <math.h>
 /**
- * jump_list - searches for a value in a sorted list of
+ * jump_list - function that searches for a value in a sorted list of
  * integers using the Jump search algorithm
  * @list: pointer to the first element of the list to search in
  * @size: number of elements in the list
  * @value: value to search for
- * Return:  first index where value is located otherwise -1
  *
-**/
+ * Return:  first index where value is located otherwise -1
+ */
+
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t leap, i = 0, j;
+	size_t jump, i = 0, j;
 	listint_t *head2, *head3;
 
 	if (!list)
 		return (NULL);
-	leap = sqrt(size);
+	jump = sqrt(size);
 	head2 = list;
 
 	while (i !=  size - 1)
 	{
 		j = 0;
 		head3 = head2;
-		while (j < leap && head2->next)
+		while (j < jump && head2->next)
 		{
 			head2 = head2->next;
 			j++;
