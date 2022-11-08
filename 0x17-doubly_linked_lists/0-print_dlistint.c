e235f174feded29438a26b466be03fe5096b5294
root <100032916+ImzyO@users.noreply.github.com>
2022-11-08 00:28:53 -0800

    debugged

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,7 +8,7 @@
  * Return: number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nbr_nodes = 0;
 	const dlistint_t *current = h;
