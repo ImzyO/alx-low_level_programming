fd6d54b9224af4a56b3a1a175218bc6e78ac9cbb
Imani <100032916+ImzyO@users.noreply.github.com>
2022-11-08 13:17:39 +0300

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,27 +7,21 @@
  * Return: pointer to the newly created hash table, NULL otherwise
  */
 
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned int k = 0;
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
+	unsigned long int i;
 
-	if (ht == NULL);
-	{
-		fprintf(stderr, "Error: malloc failed\n");
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
-	}
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
-	}
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-	for (; k < size; k++)
-		ht->array[k] = NULL;
-
-	return (ht)
+	return (ht);
 }
