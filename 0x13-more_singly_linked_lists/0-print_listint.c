bda719884c84d46a6fe66078dd347a2b6ea48284
root <100032916+ImzyO@users.noreply.github.com>
2022-11-07 22:41:18 -0800

    singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,23 +2,14 @@
 /**
  * print_listint - prints all elements of a listint_t list
  * @h: head of the linked list
+ *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
         size_t count = 0;
-        const listint_t *tmp;
+	for (count = 0; h; h = h->next, count++)
+		printf("%d\n", h->n);
 
-        tmp = h;
-        if (h == '\0')
-                {
-                        return (0);
-                }
-        while (tmp)
-                {
-                        printf("%d\n", tmp->n);
-                        count++;
-                        tmp = tmp->next;
-                 }
-        return (count);
+	return (count);
 }
