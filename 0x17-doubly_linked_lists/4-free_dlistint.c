31879bdedc60761b5d90a3364be69a98031eea44
Imani <100032916+ImzyO@users.noreply.github.com>
2022-11-08 11:39:27 +0300

    free listint

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,15 +9,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *current;
-	dlistint_t *next;
+	dlistint_t *temp;
 
-	current = head;
-
-	while (current != NULL)
+	while (head)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
